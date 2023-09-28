7bde274bf38e32b946505da59403af9496021d13
tiagotpk <ttrpk@hotmail.com>
2023-09-28 15:21:18 -0300

    changes

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -2,11 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateChildFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-const router = inject(Router)
-const authService = inject(AuthService)
 
 export const authGuard: CanActivateChildFn = (childRoute, state) => {
-  if(authService.isAuthenticated()){
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  if(!authService.isAuthenticated()){
     router.navigate(['']);
     return false;
   }
